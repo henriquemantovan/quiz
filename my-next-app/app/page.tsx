1830d13a7010546913c100de488ac370c19ed998
henriquemantovan <henrique.mantovan@usp.br>
2024-11-23 03:04:15 -0300

    1

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,24 +1,20 @@
 "use client"; // Necessário para habilitar interatividade
 
 import React from 'react';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 const QuizPage = () => {
-  const router = useRouter();
-
-  const handleStartQuiz = () => {
-    router.push('/quiz'); // Redireciona para a página do quiz
-  };
-
   return (
     <div className="quiz-container">
       <h1 className="quiz-title">🌟 Descubra qual princesa você é! 🌟</h1>
       <p className="quiz-description">
         Responda o exame ocupacional e veja qual personagem combina mais com a sua personalidade.
       </p>
-      <button className="quiz-startButton" onClick={handleStartQuiz}>
-        Iniciar Quiz
-      </button>
+      <Link href="/quiz">
+        <button className="quiz-startButton">
+          Iniciar Quiz
+        </button>
+      </Link>
     </div>
   );
 };
